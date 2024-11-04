eac66cffa6b67d7188051f6747b179605e6e04a8
JoshuaWalter2907 <joshua.walter@st.oth-regensburg.de>
2024-11-04 08:33:51 +0100

    Initial

---
+++ src/main/java/com/springboot/vitalorganize/VitalOrganizeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.vitalorganize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalOrganizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalOrganizeApplication.class, args);
+    }
+
+}
