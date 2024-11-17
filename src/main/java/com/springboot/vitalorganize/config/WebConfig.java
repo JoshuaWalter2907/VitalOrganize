6b9d6566d281daf9bb5152027ed3571b95a4dcfd
JakobSchmauss <jakob.schmauss@st.oth-regensburg.de>
2024-11-17 20:27:39 +0100

    - Structure setup - Added WebConfig - Added translations for en and de

---
+++ src/main/java/com/springboot/vitalorganize/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.springboot.vitalorganize.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver ();
+        localeResolver.setDefaultLocale(Locale.GERMAN); // Standard-`Locale` auf Deutsch setzen
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");  // URL-Parameter zur Sprachwahl
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("languages/translation"); // Anpassen, falls in `application.properties` geändert
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
