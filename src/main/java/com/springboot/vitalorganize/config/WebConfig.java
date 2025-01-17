21099627957ff9acbad4b9d4b08ab7fc6f349109
JakobSchmauss <jakob.schmauss@st.oth-regensburg.de>
2025-01-17 01:38:51 +0100

    - Vergessene imports hinzugefügt

--- src/main/java/com/springboot/vitalorganize/config/WebConfig.java
+++ src/main/java/com/springboot/vitalorganize/config/WebConfig.java
@@ -27,6 +27,10 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.security.SecureRandom;
 import java.time.LocalDate;
